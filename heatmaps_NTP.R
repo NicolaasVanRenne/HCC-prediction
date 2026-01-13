1d64f365839e413f62b057bfed510279ca77f3bb
Nicolaas Van Renne <48249789+NicolaasVanRenne@users.noreply.github.com>
2026-01-13 13:41:41 +0100

    Update heatmaps_NTP.R

--- heatmaps_NTP.R
+++ heatmaps_NTP.R
@@ -15,7 +15,7 @@
 
 #0 Load gene signature
 ############################################
-	gene.signature <- read.delim(file="training_RPM_filtered_signature_result_ordered.txt",header=T,check.names=F)
+	gene.signature <- read.delim(file="output/training_RPM_filtered_signature_result_ordered.txt",header=T,check.names=F)
 	risk.genes <- rep(0, nrow(gene.signature))
 	risk.genes[which(gene.signature[,2] > 1)] <- "high-risk genes" 
 	risk.genes[which(gene.signature[,2] < 1)] <- "low-risk genes"		
@@ -25,10 +25,13 @@
 ############################################
 	#load data
 		#extract gene expression from TPM data
-			exp.dataset <- read.delim(file="training_NTP_sorted_dataset.gct",header=T,skip=2,check.names=F)
+			exp.dataset <- read.delim(file="output/training_NTP_sorted_dataset.gct",header=T,skip=2,check.names=F)
 			rownames(exp.dataset) <- exp.dataset[,1]
 			mat <- exp.dataset[,-c(1:2)]
-	
+			
+		#read meta data
+			meta <- read.delim(file="data_files/meta_data/training_full_meta.txt",header=T,check.names=F)
+
 		#Standardize data
 			cal_z_score <- function(x){
 				(x - mean(x)) / sd(x)
@@ -43,18 +46,34 @@
 	#add annotations
 		#column annotation
 			#read cls & change 1/2/3 into poor/intermediate/good
-			cls.ordered <- read.table(file="training_NTP_PoorIntGood_predicted_sorted.cls",header=F,check.names=F, skip=2)
-			cls.ordered[cls.ordered == 1] <- "poor"
-			cls.ordered[cls.ordered == 2] <- "good"
-			cls.ordered[cls.ordered == 3] <- "intermediate"
-			cls.ordered <- unlist(cls.ordered)
-			
-			#create column annotations for complexheatmap
-			column_ha = HeatmapAnnotation(prognosis = cls.ordered, 
-				col = list(prognosis = c("poor" = "red", "intermediate" = "lightgrey", "good" = "blue")),
-			simple_anno_size = unit(0.3, "cm"),
-			show_annotation_name = c(prognosis = FALSE) 
-			)
+  			cls.ordered <- read.table(file="output/training_NTP_PoorIntGood_predicted_sorted.cls",header=F,check.names=F, skip=2)
+  			cls.ordered[cls.ordered == 1] <- "poor"
+  			cls.ordered[cls.ordered == 2] <- "good"
+  			cls.ordered[cls.ordered == 3] <- "intermediate"
+  			cls.ordered <- unlist(cls.ordered)
+
+			# Map meta data to the columns of mat.norm using SubjectName
+  			treatment <- meta$Treated[match(colnames(mat.norm), meta$SubjectName)]
+  			treatment <- factor(treatment, levels=c(0, 1), labels=c("no", "yes"))
+  			
+  			HCC <- meta$HCC_event[match(colnames(mat.norm), meta$SubjectName)]
+  			HCC <- factor(HCC, levels=c(0, 1), labels=c("no", "yes"))
+  			
+  			#create column annotations for complexheatmap
+    			# Combine column annotations
+      			column_ha <- HeatmapAnnotation(
+      			  prognosis=cls.ordered,
+      			  treated=treatment,
+      			  HCC=HCC,
+      			  col=list(
+      			    prognosis=c("poor"="red", "intermediate"="lightgrey", "good"="blue"),
+      			    treated=c("no"="snow1", "yes"="purple"),
+      			    HCC=c("no"="snow1", "yes"="salmon")
+      			  ),
+      			  simple_anno_size=unit(0.3, "cm"),
+      			  show_annotation_name=c(prognosis=FALSE, treatment=FALSE, HCC=FALSE)
+      			)
+    			
 			
 		#row annotation
 			#filter signature for genes that are NOT in the heatmap's dataset
@@ -85,15 +104,24 @@
 			heatmap_legend_param = list(title = "Z-score")
 		)
 		ht			
-			
+
+		#save pdf
+		  pdf("plots_pdf/Fig_B_heatmap_training.pdf", width = 5.5, height = 4)
+		  draw(ht, heatmap_legend_side = "right", annotation_legend_side = "right")
+		  dev.off()
+		  
+		  
 #2 Validation set heatmap
 ############################################
 	#load data
 		#extract gene expression from TPM data
-			exp.dataset <- read.delim(file="validation_NTP_sorted_dataset.gct",header=T,skip=2,check.names=F)
+			exp.dataset <- read.delim(file="output/validation_NTP_sorted_dataset.gct",header=T,skip=2,check.names=F)
 			rownames(exp.dataset) <- exp.dataset[,1]
 			mat <- exp.dataset[,-c(1:2)]
-	
+
+		#read meta data
+		  meta <- read.delim(file="data_files/meta_data/validation_full_meta.txt",header=T,check.names=F)
+			
 		#Standardize data
 			cal_z_score <- function(x){
 				(x - mean(x)) / sd(x)
@@ -108,18 +136,34 @@
 	#add annotations
 		#column annotation
 			#read cls & change 1/2/3 into poor/intermediate/good
-			cls.ordered <- read.table(file="validation_NTP_PoorIntGood_predicted_sorted.cls",header=F,check.names=F, skip=2)
+			cls.ordered <- read.table(file="output/validation_NTP_PoorIntGood_predicted_sorted.cls",header=F,check.names=F, skip=2)
 			cls.ordered[cls.ordered == 1] <- "poor"
 			cls.ordered[cls.ordered == 2] <- "good"
 			cls.ordered[cls.ordered == 3] <- "intermediate"
 			cls.ordered <- unlist(cls.ordered)
 			
-			#create column annotations for complexheatmap
-			column_ha = HeatmapAnnotation(prognosis = cls.ordered, 
-				col = list(prognosis = c("poor" = "red", "intermediate" = "lightgrey", "good" = "blue")),
-				simple_anno_size = unit(0.3, "cm"),
-				show_annotation_name = c(HCC_risk = FALSE) 
+		# Map meta data to the columns of mat.norm using SubjectName
+			treatment <- meta$Treated[match(colnames(mat.norm), meta$SubjectName)]
+			treatment <- factor(treatment, levels=c(0, 1), labels=c("no", "yes"))
+			
+			HCC <- meta$HCC_event[match(colnames(mat.norm), meta$SubjectName)]
+			HCC <- factor(HCC, levels=c(0, 1), labels=c("no", "yes"))
+			
+		#create column annotations for complexheatmap
+			# Combine column annotations
+			column_ha <- HeatmapAnnotation(
+			  prognosis=cls.ordered,
+			  treated=treatment,
+			  HCC=HCC,
+			  col=list(
+			    prognosis=c("poor"="red", "intermediate"="lightgrey", "good"="blue"),
+			    treated=c("no"="snow1", "yes"="purple"),
+			    HCC=c("no"="snow1", "yes"="salmon")
+			  ),
+			  simple_anno_size=unit(0.3, "cm"),
+			  show_annotation_name=c(prognosis=FALSE, treatment=FALSE, HCC=FALSE)
 			)
+			
 			
 		#row annotation
 			#filter signature for genes that are NOT in the heatmap's dataset
@@ -151,4 +195,9 @@
 		)
 		ht			
 			
+		#save pdf
+		pdf("plots_pdf/Fig_2E_heatmap_validation.pdf", width = 5.5, height = 4)
+		draw(ht, heatmap_legend_side = "right", annotation_legend_side = "right")
+		dev.off()
+
 
